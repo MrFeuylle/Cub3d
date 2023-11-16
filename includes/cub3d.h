e166d01e3814c44d92548a4c3a2b2afc65236ca4
Axel GIGUAIRE <agiguair@e2r10p13.clusters.42paris.fr>
2023-11-16 17:56:09 +0100

    start

---
+++ includes/cub3d.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agiguair <agiguair@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/16 17:51:33 by agiguair          #+#    #+#             */
+/*   Updated: 2023/11/16 17:52:36 by agiguair         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+# include "../libs/minilibx-linux/mlx.h"
+# include "../libs/libft/libft.h"
+# include "struct.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <X11/keysym.h>
+# include <X11/X.h>
+# include <fcntl.h>
+# include <math.h>
+# include <stdbool.h>
+
+
+#endif
